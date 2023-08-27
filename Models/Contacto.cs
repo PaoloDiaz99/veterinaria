f04c6bd6ed671ec8350b6d0220ca58b20b0b3d1c
paolodiaz99 <pda99@hotmail.com>
2023-08-27 14:50:28 -0500

    codigo inicial1

---
+++ Models/Contacto.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace veterinaria.Models
+{
+    public class Contacto
+    {
+        public string? Name { get; set; }
+
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Question { get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace veterinaria.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
